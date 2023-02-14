bd75cfd7b281459dbd7c26ff0c6d3013c6005226
Navai Suleymanli <80199998+Navai-Suleymanli@users.noreply.github.com>
2023-02-14 18:24:19 +0400

    Player Controller
    Player Controllerdir, cameradan ele de cox xosum gelmedi. Baxa bilsez baxarsioz belke daha yaxsi ede bilirsiz camerani

---
+++ Assets/Little_Ghost2/Scripts/Movement.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [Header("Player Movement")]
+    public float speed = 1.9f;
+    [Header("Player Script Cameras")]
+    public Transform playerCamera;
+    [Header("Player Animator and Gravity")]
+    public CharacterController cC;
+    [Header("Velocity")]
+    public float turnCalmTime = 0.5f;
+    public float turnCalmTime2 = 0.3f;
+    float turnCalmVelocity;
+    public float angle;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        // -1 ya da +1 qiymet alsin deye
+        float horizontal_axis = Input.GetAxisRaw("Horizontal");
+        float vertical_axis = Input.GetAxisRaw("Vertical");
+
+        // wasd basanda hansi terefe getmeli oldugu
+        Vector3 direction = new Vector3(horizontal_axis, 0f, vertical_axis).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+
+            // donub baxsin
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + playerCamera.eulerAngles.y;
+            // baxacagi yere yavas yavas donsun
+            angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnCalmVelocity, turnCalmTime);
+            // donsun
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            // hansi terefe irelilesin
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+            // irelilesin
+            cC.Move(moveDirection.normalized * speed * Time.deltaTime);
+
+
+
+        }
+
+    }
+}
