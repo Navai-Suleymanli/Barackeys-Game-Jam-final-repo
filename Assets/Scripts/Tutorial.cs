e5486819242b978fb8500a069457a2c8d25429a1
Navai Suleymanli <80199997+Khatai-Suleymanli@users.noreply.github.com>
2023-02-21 21:06:23 +0400

    Son deyisiklikler

--- Assets/Scripts/Tutorial.cs
+++ Assets/Scripts/Tutorial.cs
@@ -6,54 +6,20 @@ using TMPro;
 
 public class Tutorial : MonoBehaviour
 {
-    public TextMeshProUGUI Tutorial1;
-    public TextMeshProUGUI Tutorial2;
-    public TextMeshProUGUI Tutorial3;
-    public TextMeshProUGUI Tutorial4;
+    public GameObject foto1;
+    public GameObject foto2;
+    public GameObject foto3;
+    public GameObject foto4;
 
-    public GameObject powerup;
-    public GameObject enemy;
-
-    public Light light;
-    public Light light2;
-
-
-    private Vector3 mesafe = new Vector3(5, 0, 0);
-    private Vector3 mesaf2 = new Vector3(10,0, 0);
-    public bool created = false;
-
-    // Start is called before the first frame update
     void Start()
     {
-        Tutorial1.gameObject.SetActive(true);
-        if (!created)
-        {
-            StartCoroutine(Pass());
-        }
+
+        foto1.SetActive(true);
 
     }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Yaxsiliq"))
-        {
-            Destroy(other.gameObject);
-            Tutorial3.gameObject.SetActive(true);
-            Tutorial2.gameObject.SetActive(false);
-            light.gameObject.SetActive(true);
-            light2.gameObject.SetActive(false);
 
-            Instantiate(enemy, this.transform.position + mesaf2, this.transform.rotation);
+    
 
-        }
-
-        if (other.CompareTag("Enemy"))
-        {
-            Tutorial4.gameObject.SetActive(true);
-            Destroy(other.gameObject);
-
-        }
-
-    }
 
     // Update is called once per frame
     void Update()
@@ -62,19 +28,29 @@ public class Tutorial : MonoBehaviour
 
     }
 
-
-    IEnumerator Pass()
+    public void Pass()
     {
-        yield return new WaitForSeconds(5);
-        Tutorial1.gameObject.SetActive(false);
-        Tutorial2.gameObject.SetActive(true);
-
-        Instantiate(powerup, this.transform.position + mesafe, this.transform.rotation);
-
-        created = true;
-        
-
+        foto1.SetActive(false);
+        foto2.SetActive(true);
     }
 
+    public void Pass2()
+    {
+        foto2.SetActive(false);
+        foto3.SetActive(true);
+    }
+    public void Pass3()
+    {
+        foto3.SetActive(false);
+        foto4.SetActive(true);        
+    }
+
+    public void Pass4()
+    {
+        foto2.SetActive(false);
+        foto3.SetActive(false);
+
+        SceneManager.LoadScene(2);
+    }
 
 }
